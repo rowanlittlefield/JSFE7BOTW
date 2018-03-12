4ac7c0fd8bdfe2695a820b5769d8c7f2ea072e06
rowanlittlefield <rowanlittlefield@ymail.com>
2018-03-12 12:55:47 -0500

    Sets new background map for ChapterOne

--- chapter/chapterOne.js
+++ chapter/chapterOne.js
@@ -1,7 +1,7 @@
 function ChapterOne(sF) {
   Chapter.call(this,
     this.setupBoard(),
-    'Survive',
+    'Seize Throne',
     sF
   );
 }
@@ -10,44 +10,32 @@ ChapterOne.prototype = Object.create(Chapter.prototype);
 ChapterOne.prototype.constructor = ChapterOne;
 
 ChapterOne.prototype.setupBoard = function() {
-  let newBoard = new Board([22, 28], new Sprite(c, 352, 448, 396, 504, 'chapter/Chapter29MapSample.png', 1, 1));
+  let newBoard = new Board([21, 17], new Sprite(c, 336, 299, 378, 336.375, 'chapter/Chapter18xMapSample.png', 1, 1));
+
 
   let roy = new Roy(newBoard, new Inventory([new SwordOfSeals()]));
   let lyn = new Lyn(newBoard, new Inventory([new ManiKatti()]));
-  newBoard.placeUnit(roy, [2, 2]);
-  newBoard.placeUnit(lyn, [2, 6]);
+  newBoard.placeUnit(roy, [18, 3]);
+  newBoard.placeUnit(lyn, [17, 3]);
   newBoard.placeUnit(new Brigand(newBoard, new Inventory([new IronAxe]), 'seekAndDestroy'), [2, 3]);
-  newBoard.placeUnit(new Brigand(newBoard, new Inventory([new IronAxe]), 'TWBS'), [15, 2]);
+  newBoard.placeUnit(new Brigand(newBoard, new Inventory([new IronAxe]), 'TWBS'), [13, 2]);
 
 
 
-  for(let i = 0; i < 14; i++) {
+  for(let i = 0; i < 4; i++) {
     new Wall(newBoard, [i, 0]);
   }
 
-  for(let i = 5; i < 8; i++) {
+  for(let i = 0; i < 4; i++) {
     new Wall(newBoard, [i, 1]);
   }
 
-  for(let i = 1; i < 8; i++) {
-    new Wall(newBoard, [13, i]);
+  for(let i = 0; i < 3; i++) {
+    new Wall(newBoard, [i, 2]);
   }
 
-  for(let i = 5; i < 8; i++) {
-    new Wall(newBoard, [0, i]);
-  }
-
-  for(let i = 5; i < 8; i++) {
-    new Wall(newBoard, [3, i]);
-  }
-
-  for(let i = 5; i < 8; i++) {
-    new Wall(newBoard, [9, i]);
-  }
-
-  for(let i = 4; i < 9; i++) {
-    new Wall(newBoard, [i, 5]);
-    new Wall(newBoard, [i, 6]);
+  for(let i = 0; i < 2; i++) {
+    new Wall(newBoard, [i, 3]);
   }
 
   return newBoard;
--- createContext.js
+++ createContext.js
@@ -1,5 +1,6 @@
 var canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
+// canvas.width = window.innerWidth;
+canvas.width = 29 * 52;
 // canvas.height = window.innerHeight;
-canvas.height = 28 * 52
+canvas.height = 16 * 52;
 var c = canvas.getContext('2d');
