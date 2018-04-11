fbb761705d26cc118b86f5e4ffbe8e01a89c925e
rowanlittlefield <rowanlittlefield@ymail.com>
2018-04-10 21:43:35 -0500

    Add to lyn crit animation

--- units/playerUnits/lyn/combatAnimations/lynCritCombatAnimation.js
+++ units/playerUnits/lyn/combatAnimations/lynCritCombatAnimation.js
@@ -2,19 +2,37 @@ function LynCritCombatAnimation() {
   let sprite0 = new Sprite(c, 90, 50, 90, 50, 'units/playerUnits/lyn/combatAnimations/spriteSheets/lynCombatSprite0.png', 4, 5);
   let sprite1 = new Sprite(c, 140, 35, 140, 35, 'units/playerUnits/lyn/combatAnimations/lynCritSpriteSheet1.png', 4, 8);
   let spriteNull1 = new Sprite(c, 155, 141, 155, 141, 'units/playerUnits/lyn/combatAnimations/lynCritSpriteSheetNull.png', 16, 1);
-  let sprite2 = new Sprite(c, 155, 141, 155, 141, 'units/playerUnits/lyn/combatAnimations/lynCritSpriteSheet2.png', 80, 1);
-  let sprite3 = new Sprite(c, 155, 141, 155, 141, 'units/playerUnits/lyn/combatAnimations/lynCritSpriteSheet3.png', 80, 1);
+  let sprite2 = new Sprite(c, 155, 141, 155, 141, 'units/playerUnits/lyn/combatAnimations/lynCritSpriteSheet2.png', 4, 1);
+  let sprite3 = new Sprite(c, 155, 141, 155, 141, 'units/playerUnits/lyn/combatAnimations/lynCritSpriteSheet3.png', 4, 1);
+  let spriteNull2 = new Sprite(c, 155, 141, 155, 141, 'units/playerUnits/lyn/combatAnimations/lynCritSpriteSheetNull.png', 3, 1);
+  let sprite4 = new Sprite(c, 155, 141, 155, 141, 'units/playerUnits/lyn/combatAnimations/lynCritSpriteSheet3.png', 5, 1);
+  let spriteNull3 = new Sprite(c, 155, 141, 155, 141, 'units/playerUnits/lyn/combatAnimations/lynCritSpriteSheetNull.png', 4, 1);
+  let sprite5 = new Sprite(c, 155, 141, 155, 141, 'units/playerUnits/lyn/combatAnimations/lynCritSpriteSheet3.png', 8, 1);
+  let spriteNull4 = new Sprite(c, 155, 141, 155, 141, 'units/playerUnits/lyn/combatAnimations/lynCritSpriteSheetNull.png', 5, 1);
+  let sprite6 = new Sprite(c, 155, 141, 155, 141, 'units/playerUnits/lyn/combatAnimations/lynCritSpriteSheet3.png', 6, 1);
+  let spriteNull5 = new Sprite(c, 155, 141, 155, 141, 'units/playerUnits/lyn/combatAnimations/lynCritSpriteSheetNull.png', 7, 1);
+  let sprite7 = new Sprite(c, 155, 141, 155, 141, 'units/playerUnits/lyn/combatAnimations/lynCritSpriteSheet3.png', 7, 1);
+
 
   let positionAdjustment = {
     "3,0": [-3.6, 2.8],
+
     "4,0": [-3.6, 2.8],
+
+    "6,0": [-3.6, 2.8],
+
+    "8,0": [-3.6, 2.8],
+
+    "10,0": [-3.6, 2.8],
+
+    "12,0": [-3.6, 2.8],
 
   };
 
   let damageFrame = [0, 0];
 
-
-  let spriteQueue = [sprite0, sprite1,spriteNull1, sprite2, sprite3];
+  let spriteQueue = [sprite0, sprite1,spriteNull1, sprite2, sprite3, spriteNull2, sprite4,
+  spriteNull3, sprite5, spriteNull4, sprite6, spriteNull5, sprite7];
 
   CombatSprite.call(
     this,
