b15f55ca9ff8ba4d600fbd2685c7e3e5951a5b27
rowanlittlefield <rowanlittlefield@ymail.com>
2018-04-27 13:29:54 -0500

    Continue to refactor CombatAnimation class by delegating responsibility for rendering to the CombatAnimationBackgroundWindow class

--- animations/combatAnimation.js
+++ animations/combatAnimation.js
@@ -16,7 +16,7 @@ function CombatAnimation(combat, phaseStage) {
   let halfWidth = innerWidth / 2;
 
   this.backgroundWindow = new CombatAnimationBackgroundWindow(
-    halfWidth, this.playerUnit.name, this.enemyUnit.name
+    halfWidth, this.playerUnit, this.enemyUnit
   );
 }
 
@@ -92,72 +92,23 @@ CombatAnimation.prototype.renderAtEase = function(playerCoordinates, enemyCoordi
 //background element rendering
 
 CombatAnimation.prototype.renderBackgroundElements = function(halfWidth) {
-  // this.renderWeaponWindows(halfWidth);
-  // this.renderStatWindows(halfWidth);
-  // this.renderWeaponNames(halfWidth);
-  // this.renderHPWindows(halfWidth);
   this.backgroundWindow.render();
 }
 
-CombatAnimation.prototype.renderStatWindows = function(halfWidth) {
-  this.renderStatWindow(halfWidth, 'rgba(0, 0, 142, 1)', 350, this.playerUnit, this.enemyUnit);
-  this.renderStatWindow(halfWidth, 'rgba(255, 0, 0, 1)', -350 - 100, this.enemyUnit, this.playerUnit);
-}
-
-CombatAnimation.prototype.renderStatWindow = function(halfWidth, color, deltaX, attacker, defender) {
-  c.fillStyle = color;
-  c.fillRect(halfWidth + deltaX, 450, 100, 50);
-  renderTextWithFont("15px Arial", 'left', 'rgba(255, 255, 255, 1)',
-    `HIT`, halfWidth + deltaX, 465);
-  renderTextWithFont("15px Arial", 'left', 'rgba(255, 255, 255, 1)',
-    `DMG`, halfWidth + deltaX, 480);
-  renderTextWithFont("15px Arial", 'left', 'rgba(255, 255, 255, 1)',
-    `CRT`, halfWidth + deltaX, 495);
-  renderTextWithFont("15px Arial", 'right', 'rgba(255, 255, 255, 1)',
-    `${attacker.accuracy(defender)}`, halfWidth + deltaX + 100, 465);
-  renderTextWithFont("15px Arial", 'right', 'rgba(255, 255, 255, 1)',
-    `${attacker.damage(defender)}`, halfWidth + deltaX + 100, 480);
-  renderTextWithFont("15px Arial", 'right', 'rgba(255, 255, 255, 1)',
-    `${attacker.criticalChance(defender)}`, halfWidth + deltaX + 100, 495);
-}
-
-CombatAnimation.prototype.renderWeaponWindows = function(halfWidth) {
-  c.fillStyle = 'rgba(255, 248, 220, 1)';
-  c.fillRect(halfWidth + 1, 460, 400 - 1, 130);
-
-  c.fillStyle = 'rgba(255, 248, 220, 1)';
-  c.fillRect(halfWidth - 300 - 100, 460, 400 - 1, 130);
-}
-
-CombatAnimation.prototype.renderWeaponNames = function(halfWidth) {
-  renderTextWithFont("15px Arial", 'left', 'rgba(255, 255, 255 1)',
-   `${this.playerUnit.equippedWeapon.stats['name']}`, halfWidth + 50, 475);
-
-   renderTextWithFont("15px Arial", 'right', 'rgba(255, 255, 255 1)',
-    `${this.enemyUnit.equippedWeapon.stats['name']}`, halfWidth - 50, 475);
-}
-
-CombatAnimation.prototype.renderHPWindows = function(halfWidth, playerHP, enemyHP) {
-  renderTextWithFont("15px Arial", 'left', 'rgba(255, 255, 255 1)',
-   `${this.playerHP}`, halfWidth + 50, 550);
-
-   renderTextWithFont("15px Arial", 'right', 'rgba(255, 255, 255 1)',
-    `${this.enemyHP}`, halfWidth - 50, 550);
-}
+//private methods
 
 CombatAnimation.prototype.endAnimation = function() {
   this.phaseStage.nextStage('select unit');
 }
 
 CombatAnimation.prototype.modifyHP = function() {
-  if (this.combat.queue[this.combatQueueIndex].defender === this.playerUnit) {
-    this.playerHP = this.combat.queue[this.combatQueueIndex].defenderPostAttackHP;
+  if (this.currentAttack().defender === this.playerUnit) {
+    this.backgroundWindow.playerHP = this.currentAttack().defenderPostAttackHP;
   } else {
-    this.enemyHP = this.combat.queue[this.combatQueueIndex].defenderPostAttackHP;
+    this.backgroundWindow.enemyHP = this.currentAttack().defenderPostAttackHP;
   }
 }
 
-// Private methods
 CombatAnimation.prototype.currentAttack = function() {
   return this.combat.queue[this.combatQueueIndex];
 }
--- window/passiveWindow/combatAnimationBackgroundWindow.js
+++ window/passiveWindow/combatAnimationBackgroundWindow.js
@@ -1,7 +1,9 @@
-function CombatAnimationBackgroundWindow(halfWidth, puName, euName) {
+function CombatAnimationBackgroundWindow(halfWidth, pu, eu) {
   this.halfWidth = halfWidth;
-  this.puName = puName;
-  this.euName = euName;
+  this.pu = pu;
+  this.eu = eu;
+  this.playerHP = pu.current_hp;
+  this.enemyHP = eu.current_hp;
 }
 
 CombatAnimationBackgroundWindow.prototype = Object.create(PassiveWindow.prototype);
@@ -9,19 +11,19 @@ CombatAnimationBackgroundWindow.prototype.constructor = CombatAnimationBackgroun
 
 CombatAnimationBackgroundWindow.prototype.render = function(sF) {
   this.renderNameWindows();
-  // this.renderWeaponWindows();
-  // this.renderStatWindows();
+  this.renderWeaponWindows();
+  this.renderStatWindows();
   this.renderCentralDelineator();
-  // this.renderWeaponNames();
-  // this.renderHPWindows();
+  this.renderWeaponNames();
+  this.renderHPWindows();
 }
 
 CombatAnimationBackgroundWindow.prototype.renderNameWindows = function() {
   this.renderNameWindow(this.halfWidth, 'rgba(0, 0, 142, 1)',
-    this.puName, 250, 325);
+    this.pu.name, 250, 325);
 
   this.renderNameWindow(this.halfWidth, 'rgba(255, 0, 0, 1)',
-    this.euName, -250 - 150, -325);
+    this.eu.name, -250 - 150, -325);
 }
 
 CombatAnimationBackgroundWindow.prototype.renderNameWindow = function(halfWidth, color,
@@ -32,53 +34,53 @@ CombatAnimationBackgroundWindow.prototype.renderNameWindow = function(halfWidth,
     unitName, halfWidth + nameXCoord, 130);
 }
 
-// CombatAnimationBackgroundWindow.prototype.renderStatWindows = function(halfWidth) {
-//   this.renderStatWindow(halfWidth, 'rgba(0, 0, 142, 1)', 350, this.playerUnit, this.enemyUnit);
-//   this.renderStatWindow(halfWidth, 'rgba(255, 0, 0, 1)', -350 - 100, this.enemyUnit, this.playerUnit);
-// }
-//
-// CombatAnimationBackgroundWindow.prototype.renderStatWindow = function(halfWidth, color, deltaX, attacker, defender) {
-//   c.fillStyle = color;
-//   c.fillRect(halfWidth + deltaX, 450, 100, 50);
-//   renderTextWithFont("15px Arial", 'left', 'rgba(255, 255, 255, 1)',
-//     `HIT`, halfWidth + deltaX, 465);
-//   renderTextWithFont("15px Arial", 'left', 'rgba(255, 255, 255, 1)',
-//     `DMG`, halfWidth + deltaX, 480);
-//   renderTextWithFont("15px Arial", 'left', 'rgba(255, 255, 255, 1)',
-//     `CRT`, halfWidth + deltaX, 495);
-//   renderTextWithFont("15px Arial", 'right', 'rgba(255, 255, 255, 1)',
-//     `${attacker.accuracy(defender)}`, halfWidth + deltaX + 100, 465);
-//   renderTextWithFont("15px Arial", 'right', 'rgba(255, 255, 255, 1)',
-//     `${attacker.damage(defender)}`, halfWidth + deltaX + 100, 480);
-//   renderTextWithFont("15px Arial", 'right', 'rgba(255, 255, 255, 1)',
-//     `${attacker.criticalChance(defender)}`, halfWidth + deltaX + 100, 495);
-// }
+CombatAnimationBackgroundWindow.prototype.renderStatWindows = function() {
+  this.renderStatWindow('rgba(0, 0, 142, 1)', 350, this.pu, this.eu);
+  this.renderStatWindow('rgba(255, 0, 0, 1)', -350 - 100, this.eu, this.pu);
+}
+
+CombatAnimationBackgroundWindow.prototype.renderStatWindow = function(color, deltaX, attacker, defender) {
+  c.fillStyle = color;
+  c.fillRect(this.halfWidth + deltaX, 450, 100, 50);
+  renderTextWithFont("15px Arial", 'left', 'rgba(255, 255, 255, 1)',
+    `HIT`, this.halfWidth + deltaX, 465);
+  renderTextWithFont("15px Arial", 'left', 'rgba(255, 255, 255, 1)',
+    `DMG`, this.halfWidth + deltaX, 480);
+  renderTextWithFont("15px Arial", 'left', 'rgba(255, 255, 255, 1)',
+    `CRT`, this.halfWidth + deltaX, 495);
+  renderTextWithFont("15px Arial", 'right', 'rgba(255, 255, 255, 1)',
+    `${attacker.accuracy(defender)}`, this.halfWidth + deltaX + 100, 465);
+  renderTextWithFont("15px Arial", 'right', 'rgba(255, 255, 255, 1)',
+    `${attacker.damage(defender)}`, this.halfWidth + deltaX + 100, 480);
+  renderTextWithFont("15px Arial", 'right', 'rgba(255, 255, 255, 1)',
+    `${attacker.criticalChance(defender)}`, this.halfWidth + deltaX + 100, 495);
+}
 
 CombatAnimationBackgroundWindow.prototype.renderCentralDelineator = function() {
   c.fillStyle = 'rgba(0, 0, 0, 1)';
   c.fillRect(this.halfWidth - 1, 460, 2, 130);
 }
 
-// CombatAnimationBackgroundWindow.prototype.renderWeaponWindows = function(halfWidth) {
-//   c.fillStyle = 'rgba(255, 248, 220, 1)';
-//   c.fillRect(halfWidth + 1, 460, 400 - 1, 130);
-//
-//   c.fillStyle = 'rgba(255, 248, 220, 1)';
-//   c.fillRect(halfWidth - 300 - 100, 460, 400 - 1, 130);
-// }
-//
-// CombatAnimationBackgroundWindow.prototype.renderWeaponNames = function(halfWidth) {
-//   renderTextWithFont("15px Arial", 'left', 'rgba(255, 255, 255 1)',
-//    `${this.playerUnit.equippedWeapon.stats['name']}`, halfWidth + 50, 475);
-//
-//    renderTextWithFont("15px Arial", 'right', 'rgba(255, 255, 255 1)',
-//     `${this.enemyUnit.equippedWeapon.stats['name']}`, halfWidth - 50, 475);
-// }
-//
-// CombatAnimationBackgroundWindow.prototype.renderHPWindows = function(halfWidth, playerHP, enemyHP) {
-//   renderTextWithFont("15px Arial", 'left', 'rgba(255, 255, 255 1)',
-//    `${this.playerHP}`, halfWidth + 50, 550);
-//
-//    renderTextWithFont("15px Arial", 'right', 'rgba(255, 255, 255 1)',
-//     `${this.enemyHP}`, halfWidth - 50, 550);
-// }
+CombatAnimationBackgroundWindow.prototype.renderWeaponWindows = function() {
+  c.fillStyle = 'rgba(255, 248, 220, 1)';
+  c.fillRect(this.halfWidth + 1, 460, 400 - 1, 130);
+
+  c.fillStyle = 'rgba(255, 248, 220, 1)';
+  c.fillRect(this.halfWidth - 300 - 100, 460, 400 - 1, 130);
+}
+
+CombatAnimationBackgroundWindow.prototype.renderWeaponNames = function() {
+  renderTextWithFont("15px Arial", 'left', 'rgba(255, 255, 255 1)',
+    `${this.pu.equippedWeapon.stats['name']}`, this.halfWidth + 50, 475);
+
+  renderTextWithFont("15px Arial", 'right', 'rgba(255, 255, 255 1)',
+    `${this.eu.equippedWeapon.stats['name']}`, this.halfWidth - 50, 475);
+}
+
+CombatAnimationBackgroundWindow.prototype.renderHPWindows = function() {
+  renderTextWithFont("15px Arial", 'left', 'rgba(255, 255, 255 1)',
+   `${this.playerHP}`, this.halfWidth + 50, 550);
+
+   renderTextWithFont("15px Arial", 'right', 'rgba(255, 255, 255 1)',
+    `${this.enemyHP}`, this.halfWidth - 50, 550);
+}
