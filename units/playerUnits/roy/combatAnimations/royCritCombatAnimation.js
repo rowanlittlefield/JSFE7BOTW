35fbfedc861548ef0321a06b27be779b6e241d20
rowanlittlefield <rowanlittlefield@ymail.com>
2018-05-02 14:27:46 -0500

    Edit Roy crit animation speed

--- units/playerUnits/roy/combatAnimations/royCritCombatAnimation.js
+++ units/playerUnits/roy/combatAnimations/royCritCombatAnimation.js
@@ -9,10 +9,10 @@ function RoyCritCombatAnimation() {
   let sprite6 = new Sprite(c, 64, 68, 64, 68, 'units/playerUnits/roy/combatAnimations/spriteSheets/royCritSprite6.png', 2, 8);
   let sprite7 = new Sprite(c, 64, 61, 64, 61, 'units/playerUnits/roy/combatAnimations/spriteSheets/royCritSprite7.png', 2, 3);
   let sprite8 = new Sprite(c, 150, 55, 150, 55, 'units/playerUnits/roy/combatAnimations/spriteSheets/royCritSprite8.png', 2, 7);
-  let sprite9 = new Sprite(c, 150, 42, 150, 42, 'units/playerUnits/roy/combatAnimations/spriteSheets/royCritSprite9.png', 2, 3);
-  let sprite10 = new Sprite(c, 150, 76, 150, 76, 'units/playerUnits/roy/combatAnimations/spriteSheets/royCritSprite10.png', 2, 5);
+  let sprite9 = new Sprite(c, 150, 42, 150, 42, 'units/playerUnits/roy/combatAnimations/spriteSheets/royCritSprite9.png', 1, 3);
+  let sprite10 = new Sprite(c, 150, 76, 150, 76, 'units/playerUnits/roy/combatAnimations/spriteSheets/royCritSprite10.png', 1, 5);
   let sprite11 = new Sprite(c, 100, 31, 100, 31, 'units/playerUnits/roy/combatAnimations/spriteSheets/royCritSprite11.png', 2, 7);
-  let sprite12 = new Sprite(c, 100, 36, 100, 36, 'units/playerUnits/roy/combatAnimations/spriteSheets/royCritSprite12.png', 2, 7);
+  let sprite12 = new Sprite(c, 100, 36, 100, 36, 'units/playerUnits/roy/combatAnimations/spriteSheets/royCritSprite12.png', 4, 7);
 
   let spriteQueue = [
     sprite0, sprite1, sprite2, sprite3, sprite4, sprite5, sprite6,
