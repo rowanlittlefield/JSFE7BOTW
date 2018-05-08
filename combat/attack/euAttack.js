ab340eb8695e4fc1521960a918c2d7a9db8d9b46
rowanlittlefield <rowanlittlefield@ymail.com>
2018-05-08 15:02:52 -0500

    Remove superfluous code

--- combat/attack/euAttack.js
+++ combat/attack/euAttack.js
@@ -3,10 +3,7 @@ function EUAttack(attacker, defender, attackerCurrentHP, defenderInitialHP) {
     this, attacker, defender, attackerCurrentHP, defenderInitialHP
   );
 
-  let scaledHalfInnerWidth = (innerWidth / 2) / 52
-  let enemyWidth = 70 / 52
-  let hitCoordinates = this.playerCoordinates;
-  this.hitAnimation = new NormalDamageAnimation([hitCoordinates[0] + 1.7, hitCoordinates[1] + 2.8]);
+  this.hitAnimation = new NormalDamageAnimation([this.playerCoordinates[0] + 1.7, this.playerCoordinates[1] + 2.8]);
 }
 
 EUAttack.prototype = Object.create(Attack.prototype);
--- combat/attack/puAttack.js
+++ combat/attack/puAttack.js
@@ -3,10 +3,7 @@ function PUAttack(attacker, defender, attackerCurrentHP, defenderInitialHP) {
     this, attacker, defender, attackerCurrentHP, defenderInitialHP
   );
 
-  let scaledHalfInnerWidth = (innerWidth / 2) / 52
-  let enemyWidth = 70 / 52
-  let hitCoordinates = this.enemyCoordinates
-  this.hitAnimation = new NormalDamageAnimation([hitCoordinates[0] + 1.7, hitCoordinates[1] + 2.8]);
+  this.hitAnimation = new NormalDamageAnimation([this.enemyCoordinates[0] + 1.7, this.enemyCoordinates[1] + 2.8]);
 }
 
 PUAttack.prototype = Object.create(Attack.prototype);
