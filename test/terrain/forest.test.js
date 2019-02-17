e1c5a5a4443f77eb90645f253e96160106cb4e4d
Rowan Littlefield <30376211+rowanlittlefield@users.noreply.github.com>
2019-02-16 19:13:37 -0500

    Refactor: terrain classes (#8)
    * Convert terrain classes to ES6 syntax
    
    * Refactor Terrain and Forest class tests

--- test/terrain/forest.test.js
+++ test/terrain/forest.test.js
@@ -1,60 +1,53 @@
-// TODO: Update all terrain classes to ES6 class syntax
-// Then refactor these tests and get working. Technique below
-// for testing if constructor was called
-// only works reliably with ES6 syntax.
-
-// import Forest from '@/terrain/forest';
-// import Terrain from '@/terrain/terrain';
-// jest.mock('../../src/classes/terrain/terrain');
+import Forest from '@/terrain/forest';
+import Terrain from '@/terrain/terrain';
 
 describe('Forest', () => {
-//   const mockSetTerrainAtPositionTrue = jest.fn();
-//   mockSetTerrainAtPositionTrue.mockReturnValue(true);
-//   const mockSetTerrainAtPositionFalse = jest.fn();
-//   mockSetTerrainAtPositionFalse.mockReturnValue(false);
-//   const board = {
-//     setTerrainAtPosition: mockSetTerrainAtPositionTrue
-//   };
-//   const pos = [0,0]
-//   const forest = new Forest(board, pos);
+  let mockSetTerrainAtPosition;
+  let board;
+  let pos;
+  let forest;
 
-  describe('Constructor', () => {
-    xit('inherits from Terrain', () => {
-      expect(Forest.prototype instanceof Terrain).toBe(true);
-    });
-
-//     it('calls the Terrain constructor', () => {
-//       expect(Terrain).toHaveBeenCalledWith(board, pos);
-//     });
+  beforeEach(() => {
+    mockSetTerrainAtPosition = jest.fn();
+    mockSetTerrainAtPosition.mockReturnValue(true);
+    board = { setTerrainAtPosition: mockSetTerrainAtPosition };
+    pos = [0,0]
+    forest = new Forest(board, pos);
   });
 
-//   describe('Forest.prototype.terrainName', () => {
-//     it('returns "Forest"', () => {
-//       expect(forest.terrainName()).toBe('Forest');
-//     });
-//   });
+  describe('Constructor', () => {
+    it('inherits from Terrain', () => {
+      expect(Forest.prototype instanceof Terrain).toBe(true);
+    });
+  });
 
-//   describe('Forest.prototype.defenseBonus', () => {
-//     it('returns 1', () => {
-//       expect(forest.defenseBonus()).toBe(1);
-//     });
-//   });
+  describe('Forest.prototype.terrainName', () => {
+    it('returns "Forest"', () => {
+      expect(forest.terrainName()).toBe('Forest');
+    });
+  });
 
-//   describe('Forest.prototype.avoidBonus', () => {
-//     it('returns 20', () => {
-//       expect(forest.avoidBonus()).toBe(20);
-//     });
-//   });
+  describe('Forest.prototype.defenseBonus', () => {
+    it('returns 1', () => {
+      expect(forest.defenseBonus()).toBe(1);
+    });
+  });
 
-//   describe('Forest.prototype.healBonus', () => {
-//     it('returns 0', () => {
-//       expect(forest.healBonus()).toBe(0);
-//     });
-//   });
+  describe('Forest.prototype.avoidBonus', () => {
+    it('returns 20', () => {
+      expect(forest.avoidBonus()).toBe(20);
+    });
+  });
 
-//   describe('Forest.prototype.moveCost', () => {
-//     it('returns 2', () => {
-//       expect(forest.moveCost()).toBe(2);
-//     });
-//   });
+  describe('Forest.prototype.healBonus', () => {
+    it('returns 0', () => {
+      expect(forest.healBonus()).toBe(0);
+    });
+  });
+
+  describe('Forest.prototype.moveCost', () => {
+    it('returns 2', () => {
+      expect(forest.moveCost()).toBe(2);
+    });
+  });
 });
